321ee876c8d4e17cae5e40b7665e1191315b19ff
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-05-02 20:24:11 +0200

    changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
+import { getSession } from "next-auth/react"
+import { NextPageContext } from "next"
+import Navbar from "@/components/navbar";
 
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent: false,
+
+      }
+    }
+  }
+
+  return {
+    props:{}
+  }
+}
 
 export default function Home() {
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+      <Navbar/>
     </>
   )
 }
